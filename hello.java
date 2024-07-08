6459d675db5c879101b3a8a4a1d84a5a284da9c2
washeemansarigithub <96020753+washeemansarigithub@users.noreply.github.com>
2024-07-08 11:15:54 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+
+import from java.util.*
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("washeem");
+        System.out.println("Ansari");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextLine();
+
+    }
+}
